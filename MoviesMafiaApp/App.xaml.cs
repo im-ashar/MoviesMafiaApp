40fc525a09694e863575f94501a34780b3e1026c
im-ashar <muhammadashar.401@gmail.com>
2023-09-14 11:40:21 +0500

    Add project files.

---
+++ MoviesMafiaApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MoviesMafiaApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ MoviesMafiaApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MoviesMafiaApp;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
